2aa6979bb347047d47055a2cf0037093bd540a1e
Frangeul Nathan <seekerz17@outlook.fr>
2024-03-01 14:36:46 +0100

    done responsive (might need fixes)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,71 +14,69 @@ function Skills() {
       <h2 id="competences">Compétences</h2>
       <hr className="blackLign" />
       <div className='skillContainer'>
-        <div className="skillTree">
-          <div class="skills">
-            <h3>Front-End</h3>
+        <div className="skillBox">
+          <h3>Front-End</h3>
+          <div class="skill">
+            <div class="skill-name">HTML</div>
+            <div class="skill-bar">
+              <div class="skill-per" per="90"></div>
+            </div>
             <div class="skill">
-              <div class="skill-name">HTML</div>
+              <div class="skill-name">CSS / SCSS</div>
               <div class="skill-bar">
-                <div class="skill-per" per="90"></div>
+                <div class="skill-per" per="75"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">CSS / SCSS</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="75"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">Bootstrap / Tailwind</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="65"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">Bootstrap / Tailwind</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="65"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">JavaScript</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="70"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">JavaScript</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="70"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">ReactJS</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="70"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">ReactJS</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="70"></div>
-                </div>
+            </div>
+            <h3>Back-End</h3>
+            <div class="skill">
+              <div class="skill-name">NodeJS</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="70"></div>
               </div>
-              <h3>Back-End</h3>
-              <div class="skill">
-                <div class="skill-name">NodeJS</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="70"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">ExpressJS</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="70"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">ExpressJS</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="70"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">Sequelize</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="70"></div>
               </div>
-              <div class="skill">
-                <div class="skill-name">Sequelize</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="70"></div>
-                </div>
-              </div>
-              <div class="skill">
-                <div class="skill-name">MySQL</div>
-                <div class="skill-bar">
-                  <div class="skill-per" per="60"></div>
-                </div>
+            </div>
+            <div class="skill">
+              <div class="skill-name">MySQL</div>
+              <div class="skill-bar">
+                <div class="skill-per" per="60"></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="curriculumVitae">
-          <h3>Curriculum Vitae</h3>
-          <img src={CV} alt="" className="cv-image" />
-          <div className='button-container'>
-            <a href={cvDownload} download="Nathan-CV.pdf">
+        <div className="curriculumVitaeContainer">
+          <div className="curriculumVitae">
+            <h3>Curriculum Vitae</h3>
+            <img src={CV} alt="" className="cv-image" />
+            <a href={cvDownload} download="Nathan-CV.pdf" className="buttonContainer">
               <button>Télécharger</button>
             </a>
           </div>
