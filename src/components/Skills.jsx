dfe85d7200917f42fa05f0691a6ecdc58d1f40a8
Frangeul Nathan <seekerz17@outlook.fr>
2024-03-15 17:16:11 +0100

    updated skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,79 +2,27 @@ import "../assets/css/partials/skills/_skills.scss"
 import "../assets/css/animations/skills_animation.scss"
 import CV from "../assets/images/Nathan-CV-min.png"
 import cvDownload from "../assets/images/Nathan-CV.pdf"
-import { SkillProgressBar } from '../jquery/skillsprogressbar';
+import stacks from "../assets/images/Skills.png"
 
 
 function Skills() {
-
-  SkillProgressBar();
 
   return (
     <section className="skillSection">
       <h2 id="competences">Compétences</h2>
       <hr className="blackLign" />
-      <div className='skillContainer'>
-        <div className="skillBox">
-          <h3>Front-End</h3>
-          <div class="skill">
-            <div class="skill-name">HTML</div>
-            <div class="skill-bar">
-              <div class="skill-per" per="90"></div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">CSS / SCSS</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="75"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">Bootstrap / Tailwind</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="65"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">JavaScript</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="70"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">ReactJS</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="70"></div>
-              </div>
-            </div>
-            <h3>Back-End</h3>
-            <div class="skill">
-              <div class="skill-name">NodeJS</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="70"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">ExpressJS</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="70"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">Sequelize</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="70"></div>
-              </div>
-            </div>
-            <div class="skill">
-              <div class="skill-name">MySQL</div>
-              <div class="skill-bar">
-                <div class="skill-per" per="60"></div>
-              </div>
-            </div>
+      <div className="container">
+        <div className='skillContainer'>
+          <div className="stacks">
+            <h3>Stacks</h3>
+            <hr className="blackLign" />
+            <img src={stacks} alt="stacks" className="stacks-image"/>
           </div>
         </div>
         <div className="curriculumVitaeContainer">
           <div className="curriculumVitae">
             <h3>Curriculum Vitae</h3>
+            <hr className="blackLign" />
             <img src={CV} alt="" className="cv-image" />
             <a href={cvDownload} download="Nathan-CV.pdf" className="buttonContainer">
               <button>Télécharger</button>
